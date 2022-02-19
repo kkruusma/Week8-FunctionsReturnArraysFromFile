30fa105a79e3e7bbfee1d920515689d64ac373f1
Kaie <kkruusma@tktk.ee>
2022-02-19 22:22:02 +0200

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Valida juhuslik nali Chuck Norrise failist
+            //Salvestab naljad massiivi
+            //Kuvab suvalise nalja ekraanile
+            
+            string randomJoke;
+            randomJoke = GetRandomFromFile("chuck.txt");
+
+            Console.WriteLine($"{randomJoke}");
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\kaardiguru\samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
---
+++ FridayNightRandom/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FridayNightRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+            DisplayRandomDrink();
+            DisplayRandomMovie();
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            //pikalt välja kirjutatult 3 rida:
+            //int randomIndex = GenerateRandomIndex(foods);
+            //string randomFood = foods[randomIndex];
+            //Console.WriteLine($"Computer picked: {randomFood}");
+
+            //lühidalt saab niimoodi:
+            Console.WriteLine($"Computer picked: {foods[GenerateRandomIndex(foods)]}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+
+            Console.WriteLine($"Computer picked: {drinks[GenerateRandomIndex(drinks)]}");
+        }
+
+        private static void DisplayRandomMovie()
+        {
+            string[] movies = { "Die hard", "Home alone", "Spiderman" };
+            
+            Console.WriteLine($"Computer picked: {movies[GenerateRandomIndex(movies)]}");
+        }
+
+    }
+}
